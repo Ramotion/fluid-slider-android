5ccd88b2f928966f4cee7a8910c6b31c083cfe51
dvg4000 <dvgrishechkin@yandex.ru>
2017-11-10 10:18:40 +0300

    Fix metaball drawing and other improvements

--- fluid-slider/src/main/kotlin/com/ramotion/fluidslider/FluidSlider.kt
+++ fluid-slider/src/main/kotlin/com/ramotion/fluidslider/FluidSlider.kt
@@ -14,19 +14,18 @@ class FluidSlider : View {
 
     private companion object {
         val BAR_HEIGHT = 56
-        val BAR_CORNER_RADIUS = 10
+        val BAR_CORNER_RADIUS = 15
         val BAR_VERTICAL_OFFSET = 1.5f
 
         val SLIDER_HEIGHT = BAR_HEIGHT * 3
         val SLIDER_WIDTH = BAR_HEIGHT * 4
 
-        val ANIMATION_DURATION = 1000L
+        val ANIMATION_DURATION = 400L
 
-        val METABALL_MAX_DISTANCE = BAR_HEIGHT * 3f
-        val METABALL_SCALE_RATE = 1f
-        val METABALL_SPREAD_FACTOR = 0.5f
-        val METABALL_HANDLER_FACTOR = 0f
-        val METABALL_STROKE_WIDTH = 2
+        val METABALL_MAX_DISTANCE = BAR_HEIGHT * 5.0
+        val METABALL_SPREAD_FACTOR = 0.5
+        val METABALL_HANDLER_FACTOR = 2.4
+        val METABALL_STROKE_WIDTH = 5
 
         val TOP_CIRCLE_DIAMETER = 56
         val BOTTOM_CIRCLE_DIAMETER = 56*3.5f
@@ -96,20 +95,23 @@ class FluidSlider : View {
         offsetRectToPosition(bottomCircleRect, position)
         offsetRectToPosition(touchRect, position)
 
+        drawMetaball(canvas, paint, bottomCircleRect, topCircleRect,
+                metaballMaxDistance, METABALL_SPREAD_FACTOR, METABALL_HANDLER_FACTOR)
+
         paint.style = Paint.Style.FILL
         paint.color = colorCircle
         canvas.drawOval(topCircleRect, paint)
-        canvas.drawOval(bottomCircleRect, paint)
-        canvas.drawOval(touchRect, paint)
+//        canvas.drawOval(bottomCircleRect, paint)
+//        canvas.drawOval(touchRect, paint)
 
+        /*
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = strokeWidth
         paint.color = colorCircleStroke
         canvas.drawOval(topCircleRect, paint)
         canvas.drawOval(bottomCircleRect, paint)
         canvas.drawOval(touchRect, paint)
-
-        drawMetaball(canvas, paint)
+        */
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -149,98 +151,110 @@ class FluidSlider : View {
         rect.offsetTo(position - rect.width() / 2f, rect.top)
     }
 
-    private fun drawMetaball(canvas: Canvas, paint: Paint) {
-        val d = Math.abs(topCircleRect.centerY() - bottomCircleRect.centerY());
 
-        val radius1 = topCircleRect.width() / 2
-        val radius2 = if (d <= metaballMaxDistance) {
-            (bottomCircleRect.width() / 2) * METABALL_SCALE_RATE
-        } else {
-            bottomCircleRect.width() / 2
-        }
+    private fun getVector(radians: Double, length: Double): Pair<Double, Double> {
+        val x = (Math.cos(radians) * length)
+        val y = (Math.sin(radians) * length)
+        return x to y
+    }
 
-        Log.d("D", "d: $d, r1: $radius1, r2: $radius2, metaballMaxDistance: $metaballMaxDistance")
+    private fun getVectorLength(p1: Pair<Double, Double>, p2: Pair<Double, Double>): Double {
+        val x = p1.first - p2.first
+        val y = p1.second - p2.second
+        return Math.sqrt(x * x + y * y)
+    }
 
-        if (radius1 == 0f || radius2 == 0f) {
+    private fun drawMetaball(canvas: Canvas, paint: Paint,
+                             circle1: RectF, circle2: RectF,
+                             maxDistance: Double, v: Double, handleRate: Double)
+    {
+        val radius1 = circle1.width() / 2.0
+        val radius2 = circle2.width() / 2.0
+
+        if (radius1 == 0.0 || radius2 == 0.0) {
             return
         }
 
-        if (d > metaballMaxDistance || d < Math.abs(radius1 - radius2)) {
+        val d = getVectorLength(
+                circle1.centerX().toDouble() to circle1.centerY().toDouble(),
+                circle2.centerX().toDouble() to circle2.centerY().toDouble())
+        if (d > maxDistance || d <= Math.abs(radius1 - radius2)) {
             return
         }
 
-        val (u1, u2) = if (d < radius1 + radius2) {
-            val sqrR1 = radius1 * radius1
-            val sqrR2 = radius2 * radius2
-            val sqrD = d * d
-            Pair(Math.acos( ((sqrR1 + sqrD - sqrR2) / (2 * radius1 * d)).toDouble() ).toFloat(),
-                Math.acos( ((sqrR2 + sqrD - sqrR1) / (2 * radius2 * d)).toDouble() ).toFloat())
+        val u1: Double
+        val u2: Double
+        if (d < radius1 + radius2) { // case circles are overlapping
+            Log.d("D", "circles are overlapping")
+            u1 = Math.acos((radius1 * radius1 + d * d - radius2 * radius2) /
+                    (2 * radius1 * d))
+            u2 = Math.acos((radius2 * radius2 + d * d - radius1 * radius1) /
+                    (2 * radius2 * d))
         } else {
-            Pair(0f, 0f)
+            u1 = 0.0
+            u2 = 0.0
         }
 
-        val center1 = arrayOf(topCircleRect.centerX(), topCircleRect.centerY())
-        val center2 = arrayOf(bottomCircleRect.centerX(), bottomCircleRect.centerY())
-        val centerDiffX = (center2[0] - center1[0]).toDouble()
-        val centerDiffY = (center2[1] - center1[1]).toDouble()
+//        Log.d("D", "d: $d, u1: $u1, r1: $radius1, r2: $radius2")
 
-        val angleBetweenCenters = Math.atan2(centerDiffY, centerDiffX).toFloat()
-        val maxSpread = Math.acos(((radius1 - radius2) / d).toDouble())
+        val centerXMin = (circle2.centerX() - circle1.centerX()).toDouble()
+        val centerYMin = (circle2.centerY() - circle1.centerY()).toDouble()
 
-        val angle1a = (angleBetweenCenters + u1 + (maxSpread - u1) * METABALL_SPREAD_FACTOR).toFloat()
-        val angle1b = (angleBetweenCenters - u1 - (maxSpread - u1) * METABALL_SPREAD_FACTOR).toFloat()
-        val angle2a = (angleBetweenCenters + Math.PI - u2 - (Math.PI - u2 - maxSpread) * METABALL_SPREAD_FACTOR).toFloat()
-        val angle2b = (angleBetweenCenters - Math.PI + u2 + (Math.PI - u2 - maxSpread) * METABALL_SPREAD_FACTOR).toFloat()
+        val angle1 = Math.atan2(centerYMin, centerXMin)
+        val angle2 = Math.acos((radius1 - radius2) / d)
+        val angle1a = angle1 + u1 + (angle2 - u1) * v
+        val angle1b = angle1 - u1 - (angle2 - u1) * v
+        val angle2a = (angle1 + Math.PI - u2 - (Math.PI - u2 - angle2) * v)
+        val angle2b = (angle1 - Math.PI + u2 + (Math.PI - u2 - angle2) * v)
 
-        val p1a = getVector(angle1a, radius1).zip(center1).map { p -> p.first + p.second }
-        val p1b = getVector(angle1b, radius1).zip(center1).map { p -> p.first + p.second }
-        val p2a = getVector(angle2a, radius2).zip(center2).map { p -> p.first + p.second }
-        val p2b = getVector(angle2b, radius2).zip(center2).map { p -> p.first + p.second }
+        val p1a = getVector(angle1a, radius1).let { (it.first + circle1.centerX()) to (it.second + circle1.centerY()) }.toList()
+        val p1b = getVector(angle1b, radius1).let { (it.first + circle1.centerX()) to (it.second + circle1.centerY()) }.toList()
+        val p2a = getVector(angle2a, radius2).let { (it.first + circle2.centerX()) to (it.second + circle2.centerY()) }.toList()
+        val p2b = getVector(angle2b, radius2).let { (it.first + circle2.centerX()) to (it.second + circle2.centerY()) }.toList()
 
-        val totalRadius = radius1 + radius2
-        val dBase = Math.min(METABALL_SPREAD_FACTOR * METABALL_HANDLER_FACTOR,
-                getLength(p1a[0] - p2a[0], p1a[1] - p2a[1]) / totalRadius)
-        val d2 = dBase * Math.min(1f, d * 2 / totalRadius)
+        val totalRadius = (radius1 + radius2)
+        val d2Base = Math.min(v * handleRate, getVectorLength(p1a[0] to p1a[1], p2a[0] to p2a[1]) / totalRadius);
+
+        // case circles are overlapping:
+        val d2 = d2Base * Math.min(1.0, d * 2 / (radius1 + radius2));
 
         val r1 = radius1 * d2
         val r2 = radius2 * d2
 
-        val pi2 = (Math.PI / 2).toFloat()
-        val sp1 = getVector(angle1a - pi2, radius1)
-        val sp2 = getVector(angle2a + pi2, radius2)
-        val sp3 = getVector(angle2b - pi2, radius2)
-        val sp4 = getVector(angle1b + pi2, radius1)
+        val pi2 = Math.PI / 2
+        val sp1 = getVector(angle1a - pi2, r1).toList().map { it.toFloat() }
+        val sp2 = getVector(angle2a + pi2, r2).toList().map { it.toFloat() }
+        val sp3 = getVector(angle2b - pi2, r2).toList().map { it.toFloat() }
+        val sp4 = getVector(angle1b + pi2, r1).toList().map { it.toFloat() }
+
+        val fp1a = p1a.map { it.toFloat() }
+        val fp1b = p1b.map { it.toFloat() }
+        val fp2a = p2a.map { it.toFloat() }
+        val fp2b = p2b.map { it.toFloat() }
 
         metaballPath.reset()
-        metaballPath.moveTo(p1a[0], p1a[1])
-        metaballPath.cubicTo(p1a[0] + sp1[0], p1a[1] + sp1[1], p2a[0] + sp2[0], p2a[1] + sp2[1], p2a[0], p2a[1])
-        metaballPath.lineTo(p2b[0], p2b[1]);
-        metaballPath.cubicTo(p2b[0] + sp3[0], p2b[1] + sp3[1], p1b[0] + sp4[0], p1b[1] + sp4[1], p1b[0], p1b[1]);
-        metaballPath.lineTo(p1a[0], p1a[1]);
+        metaballPath.moveTo(fp1a[0], fp1a[1])
+        metaballPath.cubicTo(fp1a[0] + sp1[0], fp1a[1] + sp1[1], fp2a[0] + sp2[0], fp2a[1] + sp2[1], fp2a[0], fp2a[1])
+        metaballPath.lineTo(fp2b[0], fp2b[1]);
+        metaballPath.cubicTo(fp2b[0] + sp3[0], fp2b[1] + sp3[1], fp1b[0] + sp4[0], fp1b[1] + sp4[1], fp1b[0], fp1b[1]);
+        metaballPath.lineTo(fp1a[0], fp1a[1]);
         metaballPath.close();
 
         paint.style = Paint.Style.FILL
-        paint.color = colorCircle
+        paint.color = colorBar
         canvas.drawPath(metaballPath, paint);
 
+        /*
         paint.style = Paint.Style.STROKE
         paint.color = colorCircleStroke
         canvas.drawPath(metaballPath, paint);
-    }
-
-    private fun getLength(x: Float, y: Float): Float {
-        return Math.sqrt(x * x.toDouble() + y * y.toDouble()).toFloat()
-    }
-
-    private fun getVector(radians: Float, length: Float): Array<Float> {
-        val x = (Math.cos(radians.toDouble()) * length).toFloat()
-        val y = (Math.sin(radians.toDouble()) * length).toFloat()
-        return arrayOf(x, y)
+        */
     }
 
     // TODO: add distance
     private fun showLabel() {
-        val animation = ValueAnimator.ofFloat(topCircleRect.top, strokeWidth)
+        val top = barVerticalOffset - topCircleDiameter
+        val animation = ValueAnimator.ofFloat(topCircleRect.top, top)
         animation.addUpdateListener {
             topCircleRect.offsetTo(topCircleRect.left, it.animatedValue as Float)
             invalidate()
